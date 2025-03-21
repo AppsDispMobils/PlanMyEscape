c2caa54f559526c148e21a01cfa213009a69e09f
Cesar Gallardo <cesar6garo@gmail.com>
2025-03-21 01:49:51 +0100

    Multi Language

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,7 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-android-compiler:2.51.1")
     implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
-
+    implementation (libs.androidx.appcompat)
 
 }
 
