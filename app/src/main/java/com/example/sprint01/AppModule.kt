35167bfe7f7b3ce35eec3c0679fbcd00b0738e15
cesar <cgr13@alumnes.udl.cat>
2025-03-21 11:08:55 +0100

    v0.2.0

--- app/src/main/java/com/example/sprint01/AppModule.kt
+++ app/src/main/java/com/example/sprint01/AppModule.kt
@@ -1,11 +1,15 @@
 package com.example.sprint01
 
+import android.content.Context
+import android.content.SharedPreferences
+import com.example.sprint01.data.SharedPrefsManager
 import com.example.sprint01.domain.repository.TripRepository
 import com.example.sprint01.domain.repository.TripRepositoryImpl
 import com.example.sprint01.ui.viewmodel.ProgrammedTripsViewModel
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -22,6 +26,21 @@ object AppModule {
     @Singleton
     fun provideTripViewModel(trip: TripRepository): ProgrammedTripsViewModel = ProgrammedTripsViewModel(trip)
 
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(
+        @ApplicationContext context: Context
+    ): SharedPreferences =
+        context.getSharedPreferences("${BuildConfig.APPLICATION_ID}_preferences", Context.MODE_PRIVATE)
 
+
+
+    @Provides
+    @Singleton
+    fun provideSharedPrefsManager(
+        sharedPreferences: SharedPreferences,
+        @ApplicationContext context: Context
+    ): SharedPrefsManager =
+        SharedPrefsManager(sharedPreferences, context)
 
 }
